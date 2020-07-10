f4083be0778bf3387c8bd2039973f54beafb1d8c
Ricardo Lopez <richjohnlabs@gmail.com>
2020-07-10 01:10:15 +0000

    Cambios Taller 3

--- talleres/02/user-data.sh
+++ talleres/02/user-data.sh
@@ -1,18 +1,9 @@
 #!/bin/bash -xe
-
 exec > >(tee /var/log/cloud-init-output.log|logger -t user-data -s 2>/dev/console) 2>&1
-
-<<<<<<< HEAD
-sudo su -
-yum install -y python37 git
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-=======
 yum install -y python37 git
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 cd ~
->>>>>>> upstream/master
 git clone https://github.com/eloyvega/bootcamp-module-devops.git
 cd ~/bootcamp-module-devops/backend/
 pip install -r requirements.txt
